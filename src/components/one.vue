d2d7315b7406af6f5dec3870d3ab200a47083b9a
程佳麒 <32004887+eeeem@users.noreply.github.com>
2018-07-11 10:19:16 +0800

    Add files via upload

---
+++ src/components/one.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="one">
+        <h1>One 组件</h1>
+        <button @click="sendMsg">向同级组件传值</button>
+    </div>
+</template>
+
+<script>
+import bus from '../eventBus';
+export default {
+    methods: {
+        sendMsg(){
+            bus.$emit("userDefineEvent","这个组件是One")
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/two.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="two">
+        <h1>One 组件</h1>
+        <p>从ONE接受值:{{msg}}</p>
+    </div> 
+</template>
+
+<script>
+import bus from '../eventBus';
+export default {
+    data () {
+        return {
+            msg:"默认值"
+        }
+    },
+    mounted () {
+        bus.$on("userDefineEvent",(msg)=>{
+            this.msg=msg
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
